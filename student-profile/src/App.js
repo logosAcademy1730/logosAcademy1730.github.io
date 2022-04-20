d43573a071aa5fcfcb18a13545b56ca883ee1593
ihor.kurylov@clikk.app <idSNrY7I3xhB>
2022-04-20 18:43:23 +0300

    init

---
+++ student-profile/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MainPage from "./pages/main/MainPage";
+
+const App = () => {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ student-profile/src/pages/main/MainPage.jsx
@@ -0,0 +1,12 @@
+import styles from "./MainPage.module.css"
+
+const MainPage =() => {
+    console.log("main")
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+};
+
+export default MainPage;

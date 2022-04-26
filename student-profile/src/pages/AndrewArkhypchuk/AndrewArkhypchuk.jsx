03954a30cf26529e7a25bed5124411c012d4d290
Andrii Arkhypchuk <Andrewarkhypchuk@gmail.com>
2022-04-26 15:03:09 +0300

    addded andrew jsx

---
+++ student-profile/src/pages/AndrewArkhypchuk/AndrewArkhypchuk.jsx
@@ -0,0 +1,23 @@
+import classes from "./AndrewArkhypchuk.module.css";
+import myphoto from "./Andrew.jpg"
+const AndrewArkhypchuk = () => {
+    return (
+        <div>
+        <div className={classes.container}>
+            <h1>Andrew  page</h1>
+        </div>
+            <div >
+            <div>
+                <img src={myphoto} />
+            </div>
+            <div>
+    <p> Hi my name is Andrew,a have 8 months experience with Front End development,
+    Can be a great developer in your Company..!</p>
+            </div>
+
+            </div>
+        </div>
+);
+};
+
+export default AndrewArkhypchuk;
